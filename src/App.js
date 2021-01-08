8f8512689780da9a000d225b6f7740b81ceeca81
Andrea Parra <aparra001@gmail.com>
2021-01-08 18:35:04 -0300

    Tarea done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function Simplecounter(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bigCounter">
+      <div className="calendar">
+        
+      </div>
+      <div className="clock_four">{props.digitfour % 10}</div>
+      <div className="clock_three">{props.digitthree % 10}</div>
+      <div className="clock_two">{props.digittwo % 10}</div>
+      <div className="clock_one">{props.digitone % 10}</div>
     </div>
+
+
   );
 }
 
-export default App;
+export default Simplecounter;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Simplecounter from './App';
+import PropTypes from 'prop-types';
+
+
+Simplecounter.propTypes = {
+  // Puedes declarar que una propiedad es un tipo específico de JS. Por defecto, estas
+  // son todas opcionales.
+  digitone: PropTypes.number,
+  digittwo: PropTypes.number,
+  digitthree: PropTypes.number,
+  digitfour: PropTypes.number
+}
+
+let counter=0;
+setInterval(() => {
+  const clock_four=Math.floor(counter/1000);
+  const clock_three=Math.floor(counter/100);
+  const clock_two=Math.floor(counter/10);
+  const clock_one=Math.floor(counter/1);
+  console.log(clock_four, clock_three, clock_two, clock_one);
+  counter++;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Simplecounter digitone={clock_one} digittwo={clock_two} digitthree={clock_three} digitfour={clock_four}/>,
   document.getElementById('root')
 );
+},1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
