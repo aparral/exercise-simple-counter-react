49329ac0006ff687f5a7bf6754240b74dba3609b
Andrea Parra <aparra001@gmail.com>
2021-01-09 11:52:34 -0300

    Tarea done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Simplecounter from './App';
 import PropTypes from 'prop-types';
